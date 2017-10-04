a9329100984b94b3afda461930203ab4e98c1519
drkrieger <eplyushko@gmail.com>
2017-10-04 13:19:24 -0700

    set up and destination model done

---
+++ Lab17Erik/Lab17Erik/Model/Destenation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab17Erik.Model
+{
+    public class Destenation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+    }
+}
