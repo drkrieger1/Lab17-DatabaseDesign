c4254475e5122720f7a20acc6656db7ab094e2a4
drkrieger <eplyushko@gmail.com>
2017-10-04 20:27:55 -0700

    lab done

---
+++ Lab17Erik/Lab17Erik/Model/Climate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab17Erik.Model
+{
+    public class Climate
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+    }
+}
